8a7dab485f74f822b3b3dde8eaec102b9b795a73
Taichi Murai <epmt6528@gmail.com>
2021-03-17 16:16:38 -0700

    Adjusted for netlify

--- pages/blog/[id]/index.js
+++ pages/blog/[id]/index.js
@@ -33,22 +33,22 @@ export const getStaticProps = async (context) => {
 }
 
 export const getStaticPaths = async () => {
-  const res = await fetch(`${server}/apiq/posts`, {
-    method: 'GET',
-    headers: {
-      'User-Agent':
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
-      Accept: 'application/json; charset=UTF-8',
-    },
-  })
+  // const res = await fetch(`${server}/api/posts`, {
+  //   method: 'GET',
+  //   headers: {
+  //     'User-Agent':
+  //       'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
+  //     Accept: 'application/json; charset=UTF-8',
+  //   },
+  // })
 
-  const posts = await res.json()
+  // const posts = await res.json()
 
-  const ids = posts.map((post) => post.id)
-  const paths = ids.map((id) => ({ params: { id: id.toString() } }))
+  // const ids = posts.map((post) => post.id)
+  // const paths = ids.map((id) => ({ params: { id: id.toString() } }))
 
   return {
-    paths,
+    paths: [1, 2, 3, 4],
     fallback: false,
   }
 }
