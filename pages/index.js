f64a1e12432731fcc69424b2bbe90c9d445d5188
Taichi Murai <epmt6528@gmail.com>
2021-03-17 13:14:16 -0700

    Adjusted for netlify

--- pages/index.js
+++ pages/index.js
@@ -25,7 +25,15 @@ export default function Home({ posts }) {
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch(`${server}/api/posts`)
+  const res = await fetch(`${server}/api/posts`).then(async (response) => {
+    try {
+      const data = await response.json()
+      console.log('response data?', data)
+    } catch (error) {
+      console.log('Error happened here!')
+      console.error(error)
+    }
+  })
 
   const posts = await res.json()
 
