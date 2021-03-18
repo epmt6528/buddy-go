21859d17132f3e96fe843eb5e27fb981c3bf4976
Taichi Murai <epmt6528@gmail.com>
2021-03-17 20:19:03 -0700

    Adjusted for netlify

--- pages/blog/[id]/index.js
+++ pages/blog/[id]/index.js
@@ -33,14 +33,7 @@ export const getStaticProps = async (context) => {
 }
 
 export const getStaticPaths = async () => {
-  const res = await fetch(`${server}/api/posts`, {
-    method: 'GET',
-    headers: {
-      'User-Agent':
-        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
-      Accept: 'application/json; charset=UTF-8',
-    },
-  })
+  const res = await fetch(`${server}/api/posts`)
 
   const posts = await res.json()
   console.log(posts)
