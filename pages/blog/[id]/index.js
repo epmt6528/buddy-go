24e46acdd62084e0577552f3beb130b7cc1b6153
Taichi Murai <epmt6528@gmail.com>
2021-03-17 19:36:20 -0700

    Adjusted for netlify

--- pages/blog/[id]/index.js
+++ pages/blog/[id]/index.js
@@ -33,27 +33,24 @@ export const getStaticProps = async (context) => {
 }
 
 export const getStaticPaths = async () => {
-  // const res = await fetch(`${server}/api/posts`, {
-  //   method: 'GET',
-  //   headers: {
-  //     'User-Agent':
-  //       'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
-  //     Accept: 'application/json; charset=UTF-8',
-  //   },
-  // })
+  const res = await fetch(`${server}/api/posts`, {
+    method: 'GET',
+    headers: {
+      'User-Agent':
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
+      Accept: 'application/json; charset=UTF-8',
+    },
+  })
 
   // const posts = await res.json()
+  const posts = await res.text()
+  console.log(posts)
 
-  // const ids = posts.map((post) => post.id)
-  // const paths = ids.map((id) => ({ params: { id: id.toString() } }))
+  const ids = posts.map((post) => post.id)
+  const paths = ids.map((id) => ({ params: { id: id.toString() } }))
 
   return {
-    paths: [
-      { params: { id: '1' } },
-      { params: { id: '2' } },
-      { params: { id: '3' } },
-      { params: { id: '4' } },
-    ],
+    paths,
     fallback: false,
   }
 }
