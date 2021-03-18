7e9be12e967a5f7b1d1cff59523915c843097708
Taichi Murai <epmt6528@gmail.com>
2021-03-17 20:54:12 -0700

    Commented out apis to deploy on netlify

--- config/index.js
+++ config/index.js
@@ -1,6 +1,9 @@
 const dev = process.env.NODE_ENV !== 'production'
 
 // export const server = dev ? 'http://localhost:3000' : 'http://localhost:3000'
+// export const server = dev
+//   ? 'http://localhost:3000'
+//   : 'https://next-js-buddy-go-b2j0szxwu-epmt6528.vercel.app'
 export const server = dev
-  ? 'http://localhost:3000'
+  ? 'https://next-js-buddy-go-b2j0szxwu-epmt6528.vercel.app'
   : 'https://next-js-buddy-go-b2j0szxwu-epmt6528.vercel.app'
